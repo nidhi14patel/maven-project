01b7f382ddd1823ae7548717802f4b947aae973c
nidhi <niddhi14patel@gmail.com>
2025-01-20 22:02:40 +0530

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,10 +1,10 @@
 package com.mycompany.app;
 
 /**
- * Hello Nidhi!
+ * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Nidhi");
+        System.out.println("Hello world");
     }
 }
